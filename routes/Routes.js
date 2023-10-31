12e81324f5f9526f35447af16f070f81af51ad76
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-10-31 20:08:06 +0530

    Database

---
+++ routes/Routes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+
+import { addUser, getUser } from '../controller/user-controller.js';
+
+
+import upload from '../utils/upload.js';
+
+const route = express.Router();
+
+route.post('/add', addUser);
+
+
+
+export default route;
